c38bd76f77a38c659369634ff1505dff7f06c897
Neilerino <naw218@mun.ca>
2019-05-12 17:26:05 -0230

    Cleaned up some file stuff

---
+++ generate_wav.py
@@ -0,0 +1,31 @@
+import numpy as py
+
+import wave
+import struct
+
+import matplotlib.pyplot as plt
+
+def generate_wav(freq, file_name=None):
+    num_samples = 48000
+    sampling_rate = 48000.0
+    amplitude = 16000
+
+    sin_wave = [py.sin(2 * py.pi * freq * x/sampling_rate) for x in range(num_samples)]
+
+    if file_name is not None:
+
+        nframes = num_samples
+
+        comptype = 'NONE'
+        compname = 'not compressed'
+        nchannels = 1
+        sampwidth = 2
+
+        wav_file = wave.open(file_name, 'w')
+        wav_file.setparams((nchannels, sampwidth, int(sampling_rate), nframes, comptype, compname))
+
+        for s in sin_wave:
+            wav_file.writeframes(struct.pack('h', int(s*amplitude)))
+            # the pack method converts the integter to a hexadecimal value so that the wav_file can properly maniuplate it
+
+    return sin_wave
--- main.py
+++ main.py
@@ -1,31 +1,39 @@
-import numpy as py
+import numpy as np
 
 import wave
 import struct
 
 import matplotlib.pyplot as plt
 
-def generate_wav_file(freq, file_name=None):
-    num_samples = 48000
-    sampling_rate = 48000.0
-    amplitude = 16000
+from generate_wav import generate_wav
 
-    sin_wave = [py.sin(2 * py.pi * freq * x/sampling_rate) for x in range(num_samples)]
+frame_rate = 48000.0
+file = 'test.wav'
 
-    if file_name is not None:
+num_samples = 48000
+wav_file = wave.open(file, 'r')
 
-        nframes = num_samples
+data = wav_file.readframes(num_samples)
+wav_file.close
 
-        comptype = 'NONE'
-        compname = 'not compressed'
-        nchannels = 1
-        sampwidth = 2
+data = np.array(struct.unpack('{n}h'.format(n=num_samples), data))
+frequencies = np.abs(np.fft.fft(data))
+# fft returns complex numbers to get real valued numbers we need to take the abs
 
-        wav_file = wave.open(file_name, 'w')
-        wav_file.setparams((nchannels, sampwidth, int(sampling_rate), nframes, comptype, compname))
+wave = np.array(generate_wav(1000))
+wave_noise = np.array(generate_wav(50))
 
-        for s in sin_wave:
-            wav_file.writeframes(struct.pack('h', int(s*amplitude)))
-            # the pack method converts the integter to a hexadecimal value so that the wav_file can properly maniuplate it
+combined_signal = wave + wave_noise
 
-    return sin_wave
+plt.subplot(3,1,1)
+plt.plot(wave[:500])
+plt.title('Original Signal')
+
+plt.subplot(3,1,2)
+plt.plot(wave_noise[:4000])
+plt.title('Noise')
+
+plt.subplot(3,1,3)
+plt.plot(combined_signal[:3000])
+plt.title('Combined Signal')
+plt.show()
